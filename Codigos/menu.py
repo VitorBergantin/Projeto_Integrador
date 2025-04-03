dc61b84a7794519597ba7e4d3f1d177e8e6570b7
William Oliveira <william.wo@puccampinas.edu.br>
2025-04-03 20:39:52 -0300

    Adicionando o cabeçalho

--- Codigos/menu.py
+++ Codigos/menu.py
@@ -1,1 +1,4 @@
 print("Bem vindo ao sistema de sustentabilidade pessoal \ndesenvolvido pelos alunos do 1° semestre de SI da PUC-Campinas!!")
+
+print ("Wiliam Walter - RA 2500000")
+print("vitor - RA 25000000")
